7c993565fc9e6ee1ac2fff14451576148852a723
Natalie Popescu <nataliepopescu@gmail.com>
2020-06-04 02:16:09 -0400

    tool cleanup

--- bench.sh
+++ bench.sh
@@ -5,52 +5,58 @@
 # Don't scrape
 scrape=0
 # Bench BOTH unmod + mod
-unmod=0
-mod=0
-both=1
+comp="b"
 # Bench, don't test
 tst=0
-# Bench the 'inconsistent' subdir
-dir="i"
+# Bench in all subdirectories
+dir="a"
 # Some descriptive mame of this invocation
 name="sanity-2"
 
 usage () {
-    echo "Usage: $0 [-s] [-u] [-m] [-b] [-t] [-d dirname] [-n outname]"
-    echo "   -s               scrape reverse dependencies and download locally"
-    echo "   -u               only bench crates using unmodified rustc"
-    echo "   -m               only bench crates using modified rustc"
-    echo "   -b               bench crates using both unmodified and modified rustc"
-    echo "   -t               run crate tests after compiling with modified rustc"
-    echo "   -d <dir-symbol>  run only for the crates in the specified directory, where"
-    echo "                      'b' = better"
-    echo "                      'i' = inconsistent"
-    echo "                      'w' = worse"
-    echo "   -n <out-label>   name to label the output files of this invocation with"
+    echo -n ""
+    echo -n "Usage: $0 [-s] [-b <rustc-optn>] [-t] [-d <dir-symbol>] [-n <out-label>]"
+    echo -n "   -s               scrape reverse dependencies and download locally"
+    echo -n "   -b <rustc-optn>  bench crates with one of the following options:"
+    echo -n "                      'u' = unmodified rustc ONLY"
+    echo -n "                      'm' = modified rustc ONLY"
+    echo -n "                      'b' = both unmodified and modified rustc"
+    echo -n "   -t               run crate tests after compiling with modified rustc,"
+    echo -n "                    building the crate if necessary"
+    echo -n "   -d <dir-symbol>  run only for the crates in the specified directory, where"
+    echo -n "                      'b' = better"
+    echo -n "                      'i' = inconsistent"
+    echo -n "                      'w' = worse"
+    echo -n "                      'a' = all of the above"
+    echo -n "   -n <out-label>   what to label the output files of this invocation with"
+    echo -n ""
 }
 
 # Parse commandline arguments
-while getopts "sumbtd:n:h" opt
+while getopts "sb:td:n:h" opt
 do
     case "$opt" in
-    s) scrape=1
+    s)
+        scrape=1
         ;;
-    u) unmod=1
+    b)
+        comp="$OPTARG"
         ;;
-    m) mod=1
+    t)
+        tst=1
         ;;
-    b) both=1
+    d)
+        dir="$OPTARG"
         ;;
-    t) tst=1
+    n)
+        name="$OPTARG"
         ;;
-    d) dir="$OPTARG"
-        ;;
-    n) name="$OPTARG"
-        ;;
-    h) usage
+    h)
+        usage
         exit 0
         ;;
-    ?) usage
+    *)
+        usage
         exit 1
         ;;
     esac
@@ -64,23 +70,51 @@ better="$ROOT/crates/better/*/"
 inconsistent="$ROOT/crates/inconsistent/*/"
 worse="$ROOT/crates/worse/*/"
 
-if [ "$dir" = "b" ]
-then
+# Resolve which directory to run in
+case "$dir" in
+b)
     SUBDIRS="$better"
-    echo "Proceeding in 'better' subdirectory..."
-elif [ "$dir" = "i" ]
-then
+    ;;
+i)
     SUBDIRS="$inconsistent"
-    echo "Proceeding in 'inconsistent' subdirectory..."
-elif [ "$dir" = "w" ]
-then
+    ;;
+w)
     SUBDIRS="$worse"
-    echo "Proceeding in 'worse' subdirectory..."
-else
-    echo "Nonexistent directory option [ "$dir" ] specified. Please try again."
+    ;;
+a)
+    SUBDIRS=("$better" "$inconsistent" "$worse")
+    ;;
+*)
+    echo -n ""
+    echo -n "ERROR: Nonexistent directory option [ "$dir" ] passed to [ -d ]."
+    usage
     exit 1
-fi
+    ;;
+esac
 
+# Resolve which compiler version(s) to use
+unmod=0
+mod=0
+case "$comp" in
+u)
+    unmod=1
+    ;;
+m)
+    mod=1
+    ;;
+b)
+    unmod=1
+    mod=1
+    ;;
+*)
+    echo -n ""
+    echo -n "ERROR: Nonexistent compiler-version-option [ "$comp" ] passed to [ -b ]."
+    usage
+    exit 1
+    ;;
+esac
+
+# Initialize other helpful variables (mostly for naming output files)
 SUFFIX="$name"
 
 UNMOD_NAME="unmod-$SUFFIX"
@@ -104,10 +138,10 @@ fi
 
 # Step 2: Build and bench with unmodified rustc (slice bounds checks still on)
 
-if [ "$unmod" -eq 1 -o "$both" -eq 1 ]
+if [ "$unmod" -eq 1 ]
 then
     export CARGO_BUILD_RUSTC="$RUSTC_UNMOD/bin/rustc"
-    for d in $SUBDIRS
+    for d in ${SUBDIRS[@]}
     do
         cd "$d" && cargo clean && cargo bench > "$UNMOD_RES" && mv "$TARGET" "$UNMOD_TARGET_DIR" && cd "$ROOT"
     done
@@ -115,27 +149,33 @@ fi
 
 # Step 3: Build and bench with modified rustc (slice bounds checks now OFF)
 
-if [ "$mod" -eq 1 -o "$both" -eq 1 ]
+if [ "$mod" -eq 1 ]
 then
     export CARGO_BUILD_RUSTC="$RUSTC_MOD/bin/rustc"
-    for d in $SUBDIRS
+    for d in ${SUBDIRS[@]}
     do
         cd "$d" && cargo clean && cargo bench > "$MOD_RES" && mv "$TARGET" "$MOD_TARGET_DIR" && cd "$ROOT"
     done
 fi
 
-# Optional: run tests after building with modified compiler
+# Step 4: Run crate tests when compiled with modified rustc
 
-if [ "$tst" -eq 1]
+if [ "$tst" -eq 1 ]
 then
     export CARGO_BUILD_RUSTC="$RUSTC_MOD/bin/rustc"
-    for d in $SUBDIRS
+    for d in ${SUBDIRS[@]}
     do
-        cd "$d" && cargo clean && cargo test > "$MOD_TESTS" && cd "$ROOT"
+        # Need to build with modified rustc if it wasn't previously built and benched (step 3)
+        if [ "$mod" -eq 0 ]
+        then
+            cd "$d" && cargo clean && cargo test > "$MOD_TESTS" && cd "$ROOT"
+        else
+            cd "$d" && cargo test > "$MOD_TESTS" && cd "$ROOT"
+        fi
     done
 fi
 
-# Step 4: conglomerate results
+# Step 5: Conglomerate results
 
 AGGLOC="$ROOT/aggregate_bench.py"
 BENCH_NAME="bench-$SUFFIX"
@@ -143,10 +183,10 @@ DATA_FILE="$BENCH_NAME.data"
 DIFF_FILE="$BENCH_NAME.diff"
 SCRIPT_NAME="gnuplot-script"
 
-if [ "$unmod" -eq 1 -a "$mod" -eq 1 ] || [ "$both" -eq 1]
+if [ "$unmod" -eq 1 -a "$mod" -eq 1 ]
 then
     # Simple diff: Low effort to read if small set of data
-    for d in $SUBDIRS
+    for d in ${SUBDIRS[@]}
     do
         cd "$d" &&
             diff "$UNMOD_RES" "$MOD_RES" > "$DIFF_FILE" && 
@@ -154,7 +194,7 @@ then
     done
     
     # Data Aggregator for Gnuplot: Better visualization for larger sets of data
-    for d in $SUBDIRS
+    for d in ${SUBDIRS[@]}
     do
         cd "$d" &&
             python3 "$AGGLOC" "$PWD/$DATA_FILE" "$UNMOD_RES" "$MOD_RES" &&
@@ -162,7 +202,7 @@ then
     done
     
     # Gnuplot Script: Copy into crate directories for easier use
-    for d in $SUBDIRS
+    for d in ${SUBDIRS[@]}
     do
         cd "$d" &&
             cp "$ROOT/$SCRIPT_NAME" "$PWD/$SCRIPT_NAME" &&
