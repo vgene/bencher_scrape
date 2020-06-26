415b970d03a37384c48d8b8f189933a9dc00d23b
Natalie Popescu <nataliepopescu@gmail.com>
2020-06-26 18:58:31 -0400

    ok works

--- bench.sh
+++ bench.sh
@@ -17,9 +17,9 @@ name="sanity"
 output="output"
 
 UNMOD_ENV="nightly-2020-05-07-x86_64-unknown-linux-gnu"
-#NOBC_ENV="nobc"
-#NOBC_SL_ENV="nobc+sl"
-#SAFELIB_ENV="safelib"
+NOBC_ENV="nobc"
+NOBC_SL_ENV="nobc+sl"
+SAFELIB_ENV="safelib"
 
 TCHAIN_ENVS=( "$UNMOD_ENV" "$NOBC_ENV" "$NOBC_SL_ENV" "$SAFELIB_ENV" )
 
