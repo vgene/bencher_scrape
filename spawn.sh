31f23d69d9249c149ca5876f8bb1a7d2da42923b
Natalie Popescu <nataliepopescu@gmail.com>
2020-06-27 17:10:13 -0400

    add line for installing tmux remotely

--- spawn.sh
+++ spawn.sh
@@ -4,10 +4,12 @@ cp bashrc ~/.bashrc
 cp bash_profile ~/.bash_profile
 source ~/.bashrc
 
+sudo apt install tmux
+
 OUTNAME="cloudlab-output-lto"
 
 # Pre-compile
 #./bench.sh -c -o "$OUTNAME"
 
 # Run
-./bench.sh -b -r 2 -o "$OUTNAME"
+#./bench.sh -b -r 2 -o "$OUTNAME"
